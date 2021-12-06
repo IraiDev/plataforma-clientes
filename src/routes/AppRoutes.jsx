b134501f736fd35db155f55741ee0b63881650d2
Ignacio Arriagada <iarriagada@zionit.cl>
2021-12-06 09:04:34 -0300

    ultimos cambios antes de comenzar a integrar rutas

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,24 +1,24 @@
-import { Switch, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import HomeScreen from '../components/screens/HomeScreen'
 import LoginScreen from '../components/screens/LoginScreen'
 import DetailsView from '../components/views/DetailsView'
 
 function AppRoutes() {
   return (
-    <Switch>
-      <Route exact path="/login" >
+    <Routes>
+      <Route path="/login" >
         <LoginScreen />
       </Route>
-      <Route exact path="/email/:id" >
+      <Route path="/email/:id" >
         <DetailsView />
       </Route>
-      <Route exact path="/in/:id" >
+      <Route path="/in/:id" >
         <DetailsView />
       </Route>
-      <Route exact path="/" >
+      <Route path="/" >
         <HomeScreen />
       </Route>
-    </Switch>
+    </Routes>
   )
 }
 
